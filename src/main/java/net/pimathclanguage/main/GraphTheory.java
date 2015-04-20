56b71811559a673550d4b3a600e908dd90fd83a6
Haris Ziko <hziko314@gmail.com>
2015-04-20 12:46:57 +0200

    added new functions for StringCombination search and printout

--- src/main/java/net/pimathclanguage/main/GraphTheory.java
+++ src/main/java/net/pimathclanguage/main/GraphTheory.java
@@ -1,7 +1,114 @@
 package net.pimathclanguage.main;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class GraphTheory
 {
+    // for example 6! complete graph
+    public int[][][] getNewGraphEveryoneWithEveryone(int length)
+    {
+        int[][][] graph = new int[length][][];
+        
+        for (int i = 1; i < length + 1; i++)
+        {
+            graph[i - 1] = new int[2][];
+            
+            graph[i - 1][0] = new int[1];
+            graph[i - 1][0][0] = i;
+            
+            graph[i - 1][1] = new int[length - 1];
+            int pos = 0;
+            for (int j = 1; j < length + 1; j++)
+            {
+                if (i != j)
+                {
+                    graph[i - 1][1][pos] = j;
+                    pos++;
+                }
+            }
+        }
+        
+        return graph;
+    }
+    
+    // for example 1122331321 complete graph
+    public int[][][] getNewGraphStringAllCombinations(int symbols, int length)
+    {
+        int size = (int)Math.pow(symbols, length);
+        int[][][] graph = new int[size][][];
+        
+        int vertex = 0;
+        int newsize = 0;
+        for (int pos = 0; pos < size; pos++)
+        {
+            graph[pos] = new int[2][];
+            
+            graph[pos][0] = new int[1];
+            graph[pos][0][0] = pos + 1;
+            
+            newsize = (((pos + 1 - vertex) > 0) && ((vertex + symbols - pos) > 0)) ? symbols - 1 : symbols;
+            graph[pos][1] = new int[newsize];
+            
+            int counter = 0;
+            for (int pos2 = 0; pos2 < symbols; pos2++)
+            {
+                if (vertex + pos2 != pos)
+                {
+                    graph[pos][1][counter] = vertex + pos2 + 1;
+                    counter++;
+                }
+            }
+            
+            vertex = (vertex + symbols) % size;
+        }
+        
+        return graph;
+    }
+    
+    public int[] convertStringAllCombinationsToArray(int[] array, int symbols, int length, boolean withZero)
+    {
+        int size = array.length;
+        int[] output = new int[size + length - 1];
+        
+        int[] first = new int[length];
+        array[0]--;
+        
+        for (int loop = 0; loop < length; loop++)
+        {
+            first[loop] = array[0] % symbols;
+            array[0] /= symbols;
+        }
+        
+        for (int loop = 0; loop < length; loop++)
+        {
+            output[loop] = first[length - 1 - loop] + (withZero ? 0 : 1);
+        }
+        
+        for (int loop = 1; loop < size; loop++)
+        {
+            output[loop + length - 1] = (array[loop] - 1) % symbols + (withZero ? 0 : 1);
+        }
+        
+        return output;
+    }
+    
+    public String convertInt1DToString(int[] array)
+    {
+        String output = "";
+        
+        for (int pos = 0; pos < array.length; pos++)
+        {
+            if (pos > 0)
+            {
+                output += ",";
+            }
+            output += array[pos];
+        }
+        
+        return output;
+    }
+    
     public boolean checkGraphTyp0(int[][][] graph)
     {
         // iterate through all (V,E) connections 
@@ -195,5 +302,203 @@ public class GraphTheory
             }
             System.out.print("\n");
         }
+    }
+    
+    public void printFoundHamiltonCircuits(int[][] found)
+    {
+        System.out.println("Found Hamilton Circuits:");
+        for (int pos1 = 0; pos1 < found.length; pos1++)
+        {
+            System.out.print(pos1 + 1 + " Solution:");
+            for (int x : found[pos1])
+            {
+                System.out.print(", " + x);
+            }
+            System.out.println("");
+        }
+    }
+    
+    public int[][] getHamiltonCircuits(int[][][] graph, int amount)
+    {
+        int[][] foundCircuits = new int[amount][graph.length];
+        int found = 0;
+        
+        // Search for hamilton circuits in an graph
+        int pos = 0;
+        int posMax = 0;
+        int[] temp = new int[graph.length];
+        int[] temp_pos = new int[graph.length];
+        System.out.println("Get HashMap map");
+        HashMap<Integer, HashMap<Integer, Integer>> map = getMapOfGraph(graph);
+        System.out.println("Get HashMap mapSize");
+        HashMap<Integer, Integer> mapSize = getMapOfGraphSize(graph);
+        
+        temp[0] = graph[0][0][0];
+        for (int loop = 0; loop < temp_pos.length; loop++)
+        {
+            temp_pos[loop] = -1;
+        }
+        
+        System.out.println("Do the search");
+        while (pos > -1)
+        {
+//            System.out.println("temp:"+Arrays.toString(temp)+"   temp_pos:"+Arrays.toString(temp_pos));
+            temp_pos[pos]++;
+            if (temp_pos[pos] >= (int)mapSize.get(temp[pos]))
+            {
+                temp_pos[pos] = -1;
+                pos--;
+            }
+            else
+            {
+                temp[pos + 1] = map.get(temp[pos]).get(temp_pos[pos]);
+                pos++;
+                if (posMax < pos)
+                {
+                    posMax = pos;
+                    System.out.println("Best found depth: " + posMax);
+                }
+                if (pos >= temp.length - 1)
+                {
+                    boolean isFound = false;
+                    for (int[][] x : graph)
+                    {
+                        if (x[0][0] == temp[pos])
+                        {
+                            if (findValueInArray(x[1], temp[0]))
+                            {
+                                isFound = true;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (isFound && !isDuplication(temp, pos))
+                    {
+                        foundCircuits[found] = deepcopyInt1D(temp);
+                        found++;
+                        if (found >= amount)
+                        {
+                            break;
+                        }
+                    }
+                    pos--;
+                }
+                else if (isDuplication(temp, pos))
+                {
+                    pos--;
+                }
+            }
+        }
+        
+        return foundCircuits;
+    }
+    
+    public int[] deepcopyInt1D(int[] array)
+    {
+        int[] ret = new int[array.length];
+        
+        for (int pos1 = 0; pos1 < array.length; pos1++)
+        {
+            ret[pos1] = array[pos1];
+        }
+        
+        return ret;
+    }
+    
+    public int[][] deepcopyInt2D(int[][] array)
+    {
+        int[][] ret = new int[array.length][];
+        
+        for (int pos1 = 0; pos1 < array.length; pos1++)
+        {
+            ret[pos1] = new int[array[pos1].length];
+            
+            for (int pos2 = 0; pos2 < array[pos1].length; pos2++)
+            {
+                ret[pos1][pos2] = array[pos1][pos2];
+            }
+        }
+        
+        return ret;
+    }
+    
+    public int[][][] deepcopyInt3D(int[][][] array)
+    {
+        int[][][] ret = new int[array.length][][];
+        
+        for (int pos1 = 0; pos1 < array.length; pos1++)
+        {
+            ret[pos1] = new int[array[pos1].length][];
+            
+            for (int pos2 = 0; pos2 < array[pos1].length; pos2++)
+            {
+                ret[pos1][pos2] = new int[array[pos1][pos2].length];
+                for (int pos3 = 0; pos3 < array[pos1][pos2].length; pos3++)
+                {
+                    ret[pos1][pos2][pos3] = array[pos1][pos2][pos3];
+                }
+            }
+        }
+        
+        return ret;
+    }
+    
+    private HashMap<Integer, HashMap<Integer, Integer>> getMapOfGraph(int[][][] graph)
+    {
+        HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
+        
+        for (int pos1 = 0; pos1 < graph.length; pos1++)
+        {
+            map.put(graph[pos1][0][0], new HashMap<Integer, Integer>());
+        }
+        
+        for (int pos1 = 0; pos1 < graph.length; pos1++)
+        {
+            for (int pos2 = 0; pos2 < graph[pos1][1].length; pos2++)
+            {
+                map.get(graph[pos1][0][0]).put(pos2, graph[pos1][1][pos2]);
+            }
+        }
+        
+        return map;
+    }
+    private HashMap<Integer, Integer> getMapOfGraphSize(int[][][] graph)
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int loop = 0; loop < graph.length; loop++)
+        {
+            map.put(graph[loop][0][0], graph[loop][1].length);
+        }
+        
+        return map;
+    }
+    
+    private boolean isDuplication(int[] array, int pos)
+    {
+        for (int loop = 0; loop < pos; loop++)
+        {
+            if (array[pos] == array[loop])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean findValueInArray(int[] array, int value)
+    {
+        for (int x: array)
+        {
+            if (x == value)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/net/pimathclanguage/main/Main.java
+++ src/main/java/net/pimathclanguage/main/Main.java
@@ -1,25 +1,116 @@
 package net.pimathclanguage.main;
 
+import java.util.Arrays;
+
 public class Main
 {
     public static void main(String[] args)
     {
 //        System.out.println("Hello World");
         GraphTheory gt = new GraphTheory();
-        int[][][] graph = {{{1},{3,4}},
-                           {{2,6},{1,3}},
-                           {{3},{2,4,5}},
-                           {{4},{1,2,6,3}},
-                           {{5},{2,4}}};
-        boolean testing;
-        gt.printGraph(graph);
-        testing = gt.checkGraphTyp0(graph);
-        System.out.println("Graph is " + testing);
+        int[][][][] moreGraphs = 
+        {{{{1},{2,3,4}},
+        {{2},{3,4,5,6}},
+        {{3},{4,5,1}},
+        {{4},{5,1,2}},
+        {{5},{1,2,3}},
+        {{6},{1,2,3}}},
+       {{{ 1},{2}},
+        {{ 2},{3,4}},
+        {{ 3},{5,6}},
+        {{ 4},{7,8}},
+        {{ 5},{9,10}},
+        {{ 6},{11,12}},
+        {{ 7},{13,14}},
+        {{ 8},{15,16}},
+        {{ 9},{1,2}},
+        {{10},{3,4}},
+        {{11},{5,6}},
+        {{12},{7,8}},
+        {{13},{9,10}},
+        {{14},{11,12}},
+        {{15},{13,14}},
+        {{16},{15}}},
+       {{{ 1},{2}},
+        {{ 2},{3,4}},
+        {{ 3},{5,6}},
+        {{ 4},{7,8}},
+        {{ 5},{9,10}},
+        {{ 6},{11,12}},
+        {{ 7},{13,14}},
+        {{ 8},{15,16}},
+        {{ 9},{17,18}},
+        {{10},{19,20}},
+        {{11},{21,22}},
+        {{12},{23,24}},
+        {{13},{25,26}},
+        {{14},{27,28}},
+        {{15},{29,30}},
+        {{16},{31,32}},
+        {{17},{1,2}},
+        {{18},{3,4}},
+        {{19},{5,6}},
+        {{20},{7,8}},
+        {{21},{9,10}},
+        {{22},{11,12}},
+        {{23},{13,14}},
+        {{24},{15,16}},
+        {{25},{17,18}},
+        {{26},{19,20}},
+        {{27},{21,22}},
+        {{28},{23,24}},
+        {{29},{25,26}},
+        {{30},{27,28}},
+        {{31},{29,30}},
+        {{32},{31}}},
+       {{{ 1},{    2, 3}},
+        {{ 2},{ 4, 5, 6}},
+        {{ 3},{ 7, 8, 9}},
+        {{ 4},{10,11,12}},
+        {{ 5},{13,14,15}},
+        {{ 6},{16,17,18}},
+        {{ 7},{19,20,21}},
+        {{ 8},{22,23,24}},
+        {{ 9},{25,26,27}},
+        {{10},{ 1, 2, 3}},
+        {{11},{ 4, 5, 6}},
+        {{12},{ 7, 8, 9}},
+        {{13},{10,11,12}},
+        {{14},{13,   15}},
+        {{15},{16,17,18}},
+        {{16},{19,20,21}},
+        {{17},{22,23,24}},
+        {{18},{25,26,27}},
+        {{19},{ 1, 2, 3}},
+        {{20},{ 4, 5, 6}},
+        {{21},{ 7, 8, 9}},
+        {{22},{10,11,12}},
+        {{23},{13,14,15}},
+        {{24},{16,17,18}},
+        {{25},{19,20,21}},
+        {{26},{22,23,24}},
+        {{27},{25,26   }}}};
+        
+//        int[][][] graph = gt.getNewGraphEveryoneWithEveryone(100);//moreGraphs[3];
+        int[][][] graph = gt.getNewGraphStringAllCombinations(26, 4);
+        boolean testing = true;
+        //gt.printGraph(graph);
+        //testing = gt.checkGraphTyp0(graph);
+        System.out.println("Graph is " + (testing ? "Type 0" : "not Type 0"));
         
         graph = gt.convertGraphTyp1ToTyp0(graph);
         
-        gt.printGraph(graph);
-        testing = gt.checkGraphTyp0(graph);
-        System.out.println("Graph is " + testing);
+        //gt.printGraph(graph);
+        //testing = gt.checkGraphTyp0(graph);
+        System.out.println("Graph is " + (testing ? "Type 0" : "not Type 0"));
+        
+        if (testing)
+        {
+            int[][] found = gt.getHamiltonCircuits(graph, 1);
+            gt.printFoundHamiltonCircuits(found);
+            System.out.println("Convert FoundArray to StringNumberArray");
+            int[] numberArray = gt.convertStringAllCombinationsToArray(found[0], 26, 4, true);
+            System.out.println(gt.convertInt1DToString(numberArray));
+        }
     }
 }
